529c013fa18c0257552122b65ff5241baaf2e24e
Lucas <cekanlucas@gmail.com>
2020-02-25 20:16:28 +0000

    updated class for warning style when counter goes over char limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,13 @@
 
 $(document).ready(function() {
-  $(tweetArea).on('keydown', function(e){
-    let counter = $(tweetArea).val().length
-    $(charCounter).text(`${counter}`);
+  $(tweetArea).on('input', function(e){
+    // this refers to plain html node do not use
+    let textArea = $(tweetArea)
+    let counter = 140 - $(tweetArea).val().length;
+    let charCounter = textArea.siblings('.counter')
+    charCounter.text(counter);
+    counter < 0 ? 
+      charCounter.addClass('warning') : 
+      charCounter.removeClass('warning');
   })
 });
